47b3f8e7dceb9bb29e3517497730a9904acaf8a4
Dang Vinh <chubuc2411@gmail.com>
2021-11-19 19:16:15 +0700

    update

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -6,8 +7,12 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    console.log("hello world");
+app.get('/cart', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/cart.html'));
+})
+
+app.get('/products/detail', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/detailproduct.html'));
 })
 
 app.listen(port, () => {
