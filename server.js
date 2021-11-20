f6bb1da410cc1f78242835c20d6edcb736357044
Dang Vinh <chubuc2411@gmail.com>
2021-11-20 16:25:27 +0700

    update project

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
+const { MongoClient } = require('mongodb');
+const url =  'mongodb+srv://admin:admin@ecommercedata.2rahr.mongodb.net/test' || 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+const dbs_name = 'shopee-cl';
+let db = null;
+let collection = null;
+
+async function getData() {
+    await client.connect();
+    db = client.db(dbs_name);
+    collection = db.collection('products');
+}
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const template = Handlebars.compile("Name: {{name}}");
 
 app.get('/cart', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/cart.html'));
@@ -15,7 +29,8 @@ app.get('/products/detail', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/detailproduct.html'));
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
+    getData();
     console.log('Listen on port: ' + port);
 });
 
